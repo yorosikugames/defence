f10631869b84bb1f9c80b6420d8cc7c2607194d6
hyunjong.lee <hyunjong.lee.s@gmail.com>
2015-09-19 17:22:18 +0900

    remain defense > defence

--- defense/defense/Properties/Resources.Designer.cs
+++ defense/defense/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace defense.Properties {
+namespace defence.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace defense.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("defense.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("defence.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
